af10f963ddf4b2eba840abe6e396fc4e5f73a506
tlj <3215267997@qq.com>
2025-07-03 15:34:23 +0800

    第一次提交：初始化项目

--- README.md
+++ README.md
@@ -84,7 +84,7 @@
 spring:
   datasource:
     driver-class-name: com.mysql.cj.jdbc.Driver
-    url: jdbc:mysql://localhost:3306/my_db
+    url: jdbc:mysql://localhost:3306/mianshihou
     username: root
     password: 123456
 ```
@@ -172,7 +172,7 @@ PUT post_v1
 
 ```
 // 指定生成参数
-String packageName = "com.yupi.springbootinit";
+String packageName = "com.yupi.mianshihou";
 String dataName = "用户评论";
 String dataKey = "userComment";
 String upperDataKey = "UserComment";
---
+++ src/main/java/com/tlj/mianshihou/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package com.tlj.mianshihou.controller;
+
+import com.tlj.mianshihou.common.BaseResponse;
+import com.tlj.mianshihou.common.ErrorCode;
+import com.tlj.mianshihou.common.ResultUtils;
+import com.tlj.mianshihou.exception.BusinessException;
+import com.tlj.mianshihou.model.dto.postthumb.PostThumbAddRequest;
+import com.tlj.mianshihou.model.entity.User;
+import com.tlj.mianshihou.service.PostThumbService;
+import com.tlj.mianshihou.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+            HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
